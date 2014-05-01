1014f2d6513bc21282bcc9ae88c0059e88531cb4
danghvu <danghvu@gmail.com>
2014-05-01 02:41:08 -0500

    Fix bug LoopInfo not found by adding a functionpass to compute the weights

--- llvm/lib/ExecutionEngine/JITProfiling/JITProfiling.cpp
+++ llvm/lib/ExecutionEngine/JITProfiling/JITProfiling.cpp
@@ -48,19 +48,167 @@ STATISTIC(numInsertedBB, "Number of profiling basic blocks inserted");
 STATISTIC(numInsertedCallbackFunc, "Number of inserted function profiling callback");
 STATISTIC(numberOfOptimizedFunc, "Number of function was optimized");
 
+using namespace llvm;
+
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+namespace {
+  class GetEdgeWeights : public FunctionPass {
+
+    public:
+      static char ID;
+      GetEdgeWeights() : FunctionPass(ID) {}
+      GetEdgeWeights(JITProfiling *jp): FunctionPass(ID) {
+        JP = jp;
+      }
+
+      virtual void getAnalysisUsage(AnalysisUsage& AU) const {
+        AU.addRequired<UnifyFunctionExitNodes>();
+        AU.addRequired<LoopInfo>();
+      }
+
+      virtual bool runOnFunction(Function &F) {
+        LoopInfo *LI = &getAnalysis<LoopInfo>();
+        JP->ExitBB = getAnalysis<UnifyFunctionExitNodes>().getReturnBlock();
+
+        // Clear the block and edge weights if there are any values
+        JP->BlockWeights.clear();
+        JP->EdgeWeights.clear();
+
+        // Initialize the Exit->Entry edge to have a weight of 1.0
+        Edge ExitEntryEdge = std::make_pair(JP->ExitBB, &F.getEntryBlock());
+        Edge tempEdge;
+        JP->EdgeWeights[ExitEntryEdge] = 1.0;
+        float weight       = 0.0;
+        int   num_succ_nle = 0; // Number of successors that aren't loop-exit edges
+
+        SmallVector<BasicBlock*, 128> Worklist;
+        SmallPtrSet<BasicBlock*, 128> VisitedBlocks;
+
+        VisitedBlocks.insert(JP->ExitBB);
+        JP->BlockWeights[JP->ExitBB] = 1.0;
+        Worklist.push_back(&F.getEntryBlock());
+
+        // Cannot save Worklist.size() and do a static comparison because the size changes!
+        for (unsigned i = 0; i < Worklist.size(); ++i) {
+          // Pull the next basic block from the work list and get the loop information (if any exists)
+          BasicBlock* CurrentBlock = Worklist[i];
+          Loop*       CurrentLoop  = LI->getLoopFor(CurrentBlock);
+          SmallVector<ConstEdge, 16> ExitEdges;
+          weight = 0.0;
+
+          // For each predecessor of CurrentBlock
+          for (pred_iterator PI = pred_begin(CurrentBlock), PE = pred_end(CurrentBlock); PI != PE; ++PI) {
+            BasicBlock* B = *PI;
+            // The weight of a BasicBlock is the sum of the weights of its
+            // incoming edges that are not backedges (blocks that ARE in VisitedBlocks)
+            if (VisitedBlocks.count(B) == 0)
+              continue;
+            weight += JP->EdgeWeights[std::make_pair(B, CurrentBlock)];
+          }
+          // Since Exit->Entry does not actually exist as an edge in the CFG, we need to check if
+          // this block is the entry block and set it's current weight to 1
+          if (&F.getEntryBlock() == CurrentBlock)
+            weight = 1.0;
+
+          // Set the weight of the current basic block in the data structure
+          JP->BlockWeights[CurrentBlock] = weight;
+
+          // If CurrentBlock is a loop header with weight W and N = |loop_exits(B)|
+          // then each edge in loop_exits(b) has weight W/N
+          if (CurrentLoop) {
+            // Get all of the exit edges (a->b such that a is in CurrentLoop and
+            // b is not)
+            CurrentLoop->getExitEdges(ExitEdges);
+
+            if (LI->isLoopHeader(CurrentBlock)) {
+              int num_exits = ExitEdges.size();
+
+              // Set the edge weights of all of the loop exit edges and
+              for (SmallVectorImpl<ConstEdge>::iterator LEI = ExitEdges.begin(), LEE = ExitEdges.end(); LEI != LEE; ++LEI) {
+                Edge E = std::make_pair(const_cast<BasicBlock*>(LEI->first), const_cast<BasicBlock*>(LEI->second));
+                JP->EdgeWeights[E] = weight / num_exits;
+                JP->EdgePQ.push(std::make_pair(E, JP->EdgeWeights[E]));
+              }
+
+              // Let W be the weight of b times LoopMultiplier (if b is loop header)
+              weight = weight * JP->LoopMultiplier;
+              JP->BlockWeights[CurrentBlock] = weight;
+            }
+          }
+
+          int num_succ = 0;
+          for (succ_iterator SI = succ_begin(CurrentBlock), SE = succ_end(CurrentBlock); SI != SE; ++SI) {
+            num_succ += 1;
+          }
+
+          int   num_succ_lee = 0;   // Number of loop-edge exits
+          float w_e          = 0.0; // loop-edge weights to successors of b
+
+          if (CurrentLoop) {
+            for (SmallVectorImpl<ConstEdge>::iterator LEI = ExitEdges.begin(), LEE = ExitEdges.end(); LEI != LEE; ++LEI) {
+              Edge E = std::make_pair(const_cast<BasicBlock*>((*LEI).first), const_cast<BasicBlock*>((*LEI).second));
+              // Get w_e = sum of weight of outgoing edges of b that are loop-exit edges
+              if (E.first == CurrentBlock) {
+                w_e += JP->EdgeWeights[E];
+                ++num_succ_lee;
+              }
+            }
+          }
+
+          // Number of successors that aren't loop-exit edges
+          num_succ_nle = num_succ - num_succ_lee;
+
+          // 3b) Each outgoing edge of b that is not a loop-exit edge has a weight (w-w_e)/N
+          //     where N is the number of outgoing edges of b that are not loop-exit edges
+          for (succ_iterator SI = succ_begin(CurrentBlock),
+              SE = succ_end(CurrentBlock); SI != SE; ++SI) {
+            BasicBlock* b1 = CurrentBlock;
+            BasicBlock* b2 = *SI;
+            if (VisitedBlocks.count(*SI) == 0) {
+              Worklist.push_back(*SI);
+              VisitedBlocks.insert(*SI);
+            }
+
+            if (JP->ExitEdgesContains(ExitEdges, std::make_pair(b1, b2))) {
+              continue;
+            }
+            Edge E = std::make_pair(b1, b2);
+            JP->EdgeWeights[E] = (weight - w_e) / (float)(num_succ_nle);
+            JP->EdgePQ.push(std::make_pair(E, JP->EdgeWeights[E]));
+          }
+
+          VisitedBlocks.insert(CurrentBlock);
+        }
+
+        return false;
+      }
+
+    private:
+      JITProfiling* JP;
+  };
+}
+
+char GetEdgeWeights::ID = 0;
+static RegisterPass<GetEdgeWeights> X("getedgeweights", "For getting weights of edge in a CFG (required only for JITProfiling)", false, false);
+
 namespace llvm {
 
-// Constructor for the object
-JITProfiling::JITProfiling(Function* Func, ExecutionEngine* JIT) {
-  F = Func;
-  TheJIT = JIT;
-  previouslyExecuted = false;
-  alreadyRemovedInsts = false;
-}
+  // Constructor for the object
+  JITProfiling::JITProfiling(Function* Func, ExecutionEngine* JIT) {
+    F = Func;
+    TheJIT = JIT;
+    previouslyExecuted = false;
+    alreadyRemovedInsts = false;
+  }
 
-bool JITProfiling::hasPInstruction() {
-  return previouslyExecuted && !alreadyRemovedInsts;
-}
+  bool JITProfiling::hasPInstruction() {
+    return previouslyExecuted && !alreadyRemovedInsts;
+  }
 
 bool JITProfiling::run(bool changed) {
   // If the function only has a single basic block, no profiling is needed (profiling
@@ -69,18 +217,6 @@ bool JITProfiling::run(bool changed) {
     DEBUG( dbgs() <<  "*** Function only has one basic block - no profiling code needed ***\n" );
     return false;
   }
-
-  // Create a pass manager to run the LoopInfo and UnifyFunctionExitNodes passes
-  // which are required for adding the profiling
-  FunctionPassManager* Manager = new FunctionPassManager(F->getParent());
-  this->LI = new LoopInfo();
-  Manager->add(this->LI);
-  Pass* UFEN = createUnifyFunctionExitNodesPass();
-  Manager->add(UFEN);
-  Manager->doInitialization();
-  Manager->run(*F);
-  Manager->doFinalization();
-  this->ExitBB = static_cast<UnifyFunctionExitNodes*>(UFEN)->getReturnBlock();
 
   bool results;
   // If MaxSpanningTree is not allocated, this is the first time we are running
@@ -114,7 +250,7 @@ bool JITProfiling::run(bool changed) {
   initializeEdgeCounts();
   previouslyExecuted = true;
   alreadyRemovedInsts = false;
-  printInsertionEdges();
+  DEBUG( printInsertionEdges() );
 
   return results;
 }
@@ -160,9 +296,11 @@ void* JITProfiling::CallbackFunction(BasicBlock* B) {
   unsigned threshold = (TheJIT->getProfileData())->getThresholdT1();
   EdgeCounts[E] += 1;
   DEBUG( dbgs() << "Threshold: " << threshold << "  | Edge count: " );
-  printEdge(E, EdgeCounts[E]);
+  DEBUG( printEdge(E, EdgeCounts[E]) );
 
-  if (EdgeCounts[E] >= threshold) {
+  unsigned int stat = EdgeCounts[E];
+  // % 8 so that it only updateCounts() occationally
+  if (stat == threshold || (stat > threshold && stat % 8 == 0)) {
     // Update counts and keep track of basic blocks above threshold
     SmallPtrSet<BasicBlock*, 8> HotBlocks;
     updateCounts(HotBlocks, threshold);
@@ -176,8 +314,6 @@ void* JITProfiling::CallbackFunction(BasicBlock* B) {
 
     DEBUG( dbgs() << F->getName() << "[JITProfiling callback] results:\n" );
     DEBUG( F->dump() );
-
-    //TheJIT->
 
     // Need to make sure nothing is done in callback once instructions are removed!
     // Since the machine code for callback will still remain until the function is
@@ -646,163 +782,64 @@ void JITProfiling::constructMaxSpanTree() {
 // Note that unreachable basic blocks are not given weights based on the structure of the algorithm
 // This means that any edge that is unreachable will not have profiling instructions inserted
 void JITProfiling::getWeights() {
-  // Clear the block and edge weights if there are any values
-  BlockWeights.clear();
-  EdgeWeights.clear();
-
-  // Initialize the Exit->Entry edge to have a weight of 1.0
-  Edge ExitEntryEdge = std::make_pair(ExitBB, &F->getEntryBlock());
-  Edge tempEdge;
-  EdgeWeights[ExitEntryEdge] = 1.0;
-  float weight       = 0.0;
-  int   num_succ_nle = 0; // Number of successors that aren't loop-exit edges
-
-  SmallVector<BasicBlock*, 128> Worklist;
-  SmallPtrSet<BasicBlock*, 128> VisitedBlocks;
-  VisitedBlocks.insert(ExitBB);
-  BlockWeights[ExitBB] = 1.0;
-  Worklist.push_back(&F->getEntryBlock());
-
-  // Cannot save Worklist.size() and do a static comparison because the size changes!
-  for (unsigned i = 0; i < Worklist.size(); ++i) {
-    // Pull the next basic block from the work list and get the loop information (if any exists)
-    BasicBlock* CurrentBlock = Worklist[i];
-    Loop*       CurrentLoop  = LI->getLoopFor(CurrentBlock);
-    SmallVector<ConstEdge, 16> ExitEdges;
-    weight = 0.0;
-
-    // For each predecessor of CurrentBlock
-    for (pred_iterator PI = pred_begin(CurrentBlock), PE = pred_end(CurrentBlock); PI != PE; ++PI) {
-      BasicBlock* B = *PI;
-      // The weight of a BasicBlock is the sum of the weights of its
-      // incoming edges that are not backedges (blocks that ARE in VisitedBlocks)
-      if (VisitedBlocks.count(B) == 0)
-        continue;
-      weight += EdgeWeights[std::make_pair(B, CurrentBlock)];
-    }
-    // Since Exit->Entry does not actually exist as an edge in the CFG, we need to check if
-    // this block is the entry block and set it's current weight to 1
-    if (&F->getEntryBlock() == CurrentBlock)
-      weight = 1.0;
-
-    // Set the weight of the current basic block in the data structure
-    BlockWeights[CurrentBlock] = weight;
-
-    // If CurrentBlock is a loop header with weight W and N = |loop_exits(B)|
-    // then each edge in loop_exits(b) has weight W/N
-    if (CurrentLoop) {
-      // Get all of the exit edges (a->b such that a is in CurrentLoop and
-      // b is not)
-      CurrentLoop->getExitEdges(ExitEdges);
-
-      if (LI->isLoopHeader(CurrentBlock)) {
-        int num_exits = ExitEdges.size();
-
-        // Set the edge weights of all of the loop exit edges and
-        for (SmallVectorImpl<ConstEdge>::iterator LEI = ExitEdges.begin(), LEE = ExitEdges.end(); LEI != LEE; ++LEI) {
-          Edge E = std::make_pair(const_cast<BasicBlock*>(LEI->first), const_cast<BasicBlock*>(LEI->second));
-          EdgeWeights[E] = weight / num_exits;
-          EdgePQ.push(std::make_pair(E, EdgeWeights[E]));
-        }
-
-        // Let W be the weight of b times LoopMultiplier (if b is loop header)
-        weight = weight * LoopMultiplier;
-        BlockWeights[CurrentBlock] = weight;
-      }
-    }
-
-    int num_succ = 0;
-    for (succ_iterator SI = succ_begin(CurrentBlock), SE = succ_end(CurrentBlock); SI != SE; ++SI) {
-      num_succ += 1;
-    }
-
-    int   num_succ_lee = 0;   // Number of loop-edge exits
-    float w_e          = 0.0; // loop-edge weights to successors of b
-
-    if (CurrentLoop) {
-      for (SmallVectorImpl<ConstEdge>::iterator LEI = ExitEdges.begin(), LEE = ExitEdges.end(); LEI != LEE; ++LEI) {
-        Edge E = std::make_pair(const_cast<BasicBlock*>((*LEI).first), const_cast<BasicBlock*>((*LEI).second));
-        // Get w_e = sum of weight of outgoing edges of b that are loop-exit edges
-        if (E.first == CurrentBlock) {
-          w_e += EdgeWeights[E];
-          ++num_succ_lee;
-        }
-      }
-    }
-
-    // Number of successors that aren't loop-exit edges
-    num_succ_nle = num_succ - num_succ_lee;
-
-    // 3b) Each outgoing edge of b that is not a loop-exit edge has a weight (w-w_e)/N
-    //     where N is the number of outgoing edges of b that are not loop-exit edges
-    for (succ_iterator SI = succ_begin(CurrentBlock),
-         SE = succ_end(CurrentBlock); SI != SE; ++SI) {
-      BasicBlock* b1 = CurrentBlock;
-      BasicBlock* b2 = *SI;
-      if (VisitedBlocks.count(*SI) == 0) {
-        Worklist.push_back(*SI);
-        VisitedBlocks.insert(*SI);
-      }
-
-      if (ExitEdgesContains(ExitEdges, std::make_pair(b1, b2))) {
-        continue;
-      }
-      Edge E = std::make_pair(b1, b2);
-      EdgeWeights[E] = (weight - w_e) / (float)(num_succ_nle);
-      EdgePQ.push(std::make_pair(E, EdgeWeights[E]));
-    }
-
-    VisitedBlocks.insert(CurrentBlock);
-  }
+  // Create a pass manager to run the LoopInfo and UnifyFunctionExitNodes passes
+  // which are required for adding the profiling
+  FunctionPassManager* Manager = new FunctionPassManager(F->getParent());
+  Pass* UFEN = createUnifyFunctionExitNodesPass();
+  Manager->add(UFEN);
+  Manager->add(new GetEdgeWeights(this));
+  Manager->doInitialization();
+  Manager->run(*F);
+  Manager->doFinalization();
 }
 
 void JITProfiling::printAllWeights() {
-  DEBUG( dbgs() << "**** BlockWeights ****\n" );
+  dbgs() << "**** BlockWeights ****\n";
   for (BlockWeightMap::iterator DI = BlockWeights.begin(), DE = BlockWeights.end(); DI != DE; ++DI) {
-    DEBUG( dbgs() <<  DI->first->getName() << " " <<  DI->second << "\n" );
+    dbgs() <<  DI->first->getName() << " " <<  DI->second << "\n";
   }
 
-  DEBUG( dbgs() << "***** EdgeWeights *****\n" );
+  dbgs() << "***** EdgeWeights *****\n";
   for (EdgeWeightMap::iterator DI = EdgeWeights.begin(), DE = EdgeWeights.end(); DI != DE; ++DI) {
     printEdge(DI->first, DI->second);
   }
 }
 
 void JITProfiling::printMaxSpanTree() {
-  DEBUG( dbgs() << "**** Max Spanning Tree ****\n" );
+  dbgs() << "**** Max Spanning Tree ****\n";
   for (SetVector<Edge>::iterator I = MaxSpanningTree.begin(), E = MaxSpanningTree.end(); I != E; ++I) {
     printEdge(*I);
   }
 }
 
 void JITProfiling::printInsertionEdges() {
-  DEBUG( dbgs() << "**** Insertion Edges ****\n" );
+  dbgs() << "**** Insertion Edges ****\n";
   for (EdgeSet::iterator I = ProfileEdges.begin(), E = ProfileEdges.end(); I != E; ++I) {
     printEdge(*I);
   }
 }
 
 void JITProfiling::printEdge(Edge E) {
-  DEBUG( dbgs() <<  E.first->getName() << " -> " <<  E.second->getName() << "\n" );
+  dbgs() <<  E.first->getName() << " -> " <<  E.second->getName() << "\n";
 }
 
 void JITProfiling::printEdge(Edge E, float F) {
-  DEBUG( dbgs() <<  E.first->getName() << ": " << E.second->getName() << " " << F << "\n" );
+  dbgs() <<  E.first->getName() << ": " << E.second->getName() << " " << F << "\n";
 }
 
 void JITProfiling::printEdge(Edge E, unsigned U) {
-  DEBUG( dbgs() <<  E.first->getName() << ": " << E.second->getName() << " " << U << "\n" );
+  dbgs() <<  E.first->getName() << ": " << E.second->getName() << " " << U << "\n" ;
 }
 
 void JITProfiling::printEdgeCounts() {
-  DEBUG( dbgs() << "**** Edge Counts ****\n" );
+  dbgs() << "**** Edge Counts ****\n";
   for (EdgeCountSet::iterator I = EdgeCounts.begin(), E = EdgeCounts.end(); I != E; ++I)
     printEdge(I->first, I->second);
 }
 
 void JITProfiling::printBlockCounts() {
-  DEBUG( dbgs() << "**** Block Counts ****\n" );
+  dbgs() << "**** Block Counts ****\n";
   for (BlockCountSet::iterator I = BlockCounts.begin(), E = BlockCounts.end(); I != E; ++I)
-    DEBUG( dbgs() << I->first->getName() << ": " << I->second );
+    dbgs() << I->first->getName() << ": " << I->second << "\n";
 }
 }
