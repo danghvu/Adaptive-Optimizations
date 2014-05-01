c00866ea1c195104b7834ee41297b7d2c0bb3bab
Alex Brooks <brooks8@illinois.edu>
2014-05-01 16:45:41 -0500

    Updated function profiling to pass LLVM function pointer as well

--- llvm/lib/ExecutionEngine/JITProfiling/JITFunctionProfiling.cpp
+++ llvm/lib/ExecutionEngine/JITProfiling/JITFunctionProfiling.cpp
@@ -17,21 +17,12 @@
 #define DEBUG_TYPE "jitfprofiling"
 // this name is used when -stats is used
 
-#include "JITProfiling.h"
 #include "llvm/ExecutionEngine/ExecutionEngine.h"
-#include "llvm/ExecutionEngine/OnlineProfileData.h"
-#include "llvm/Transforms/IPO.h"
-#include "llvm/Transforms/Utils/UnifyFunctionExitNodes.h"
-#include "llvm/Transforms/Utils/LoopUtils.h"
-#include "llvm/Transforms/IPO.h"
-#include "llvm/Analysis/LoopInfo.h"
-#include "llvm/Analysis/LoopPass.h"
+#include "llvm/ExecutionEngine/JITProfileData.h"
 #include "llvm/ADT/DepthFirstIterator.h"
-#include "llvm/ADT/SmallPtrSet.h"
-#include "llvm/ADT/SetVector.h"
 #include "llvm/ADT/SmallVector.h"
 #include "llvm/ADT/Statistic.h"
-#include "llvm/ADT/DenseMap.h"
+#include "llvm/IR/Function.h"
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/IntrinsicInst.h"
@@ -39,32 +30,26 @@
 #include "llvm/Support/CFG.h"
 #include "llvm/Support/InstIterator.h"
 #include "llvm/Support/Debug.h"
+#include "llvm/Support/raw_ostream.h"
 #include <stdio.h>
-#include <queue>
 #include <string>
 #include <functional>
 
 using namespace llvm;
 
-#include "llvm/Pass.h"
-#include "llvm/IR/Function.h"
-#include "llvm/Support/raw_ostream.h"
-
 STATISTIC(numInsertedCallbackFunc, "Number of inserted function profiling callback");
 
 namespace {
-
-  void *MyCallbackFunctionT1(JITProfiling *BP) {
-    return BP->CallbackFunction();
+  void* JITFunctionCallback(JITProfilingData* JPD, Function* Func) {
+    return JPD->FunctionCallback(Func);
   }
 
   class JITFunctionProfiling : public FunctionPass {
 
     public:
       static char ID;
-      JITFunctionProfiling() : FunctionPass(ID) {}
-      JITFunctionProfiling(JITProfiling *jp) : FunctionPass(ID) {
-        JP = jp;
+      JITFunctionProfiling(JITProfilingData* _JPD) : FunctionPass(ID) {
+        JPD = _JPD;
       }
 
       ~JITFunctionProfiling() {
@@ -75,8 +60,7 @@ namespace {
         callBackInst.clear();
       }
 
-      virtual void getAnalysisUsage(AnalysisUsage& AU) const {
-      }
+      virtual void getAnalysisUsage(AnalysisUsage& AU) const {}
 
       virtual bool runOnFunction(Function &F) {
         numInsertedCallbackFunc++;
@@ -87,8 +71,9 @@ namespace {
         //  Create a vector of the argument types
         std::vector<Type*> FunctionArgsTy;
         FunctionArgsTy.push_back(VoidPointerTy);
+        FunctionArgsTy.push_back(VoidPointerTy);
 
-        // Create the function type: PointerTy func(PointerTy)
+        // Create the function type: PointerTy func(PointerTy, PointerTy)
         FunctionType* FunctionTy = FunctionType::get(VoidPointerTy, FunctionArgsTy, false);
 
         // Create the function-pointer type
@@ -96,40 +81,47 @@ namespace {
 
         // Insert the inttoptr instructions for the function callback and this function pass
         // (both never change)
-        intptr_t FP      = reinterpret_cast<intptr_t>(MyCallbackFunctionT1);
-        intptr_t ThisObj = reinterpret_cast<intptr_t>(JP);
+        intptr_t FP     = reinterpret_cast<intptr_t>(JITFunctionCallback);
+        intptr_t JPDObj = reinterpret_cast<intptr_t>(JPD);
+        intptr_t Func   = reinterpret_cast<intptr_t>(*F);
 
-        Value* fnptr = ConstantInt::get(IntegerType::get(F.getContext(), sizeof(intptr_t)*CHAR_BIT), APInt(sizeof(intptr_t)*CHAR_BIT, FP));
-        Value* bpptr = ConstantInt::get(IntegerType::get(F.getContext(), sizeof(intptr_t)*CHAR_BIT), APInt(sizeof(intptr_t)*CHAR_BIT, ThisObj));
+        Value* CallbackVal = ConstantInt::get(IntegerType::get(F.getContext(), sizeof(intptr_t)*CHAR_BIT), APInt(sizeof(intptr_t)*CHAR_BIT, FP));
+        Value* JPDVal      = ConstantInt::get(IntegerType::get(F.getContext(), sizeof(intptr_t)*CHAR_BIT), APInt(sizeof(intptr_t)*CHAR_BIT, JPDObj));
+        Value* FuncVal     = ConstantInt::get(IntegerType::get(F.getContext(), sizeof(intptr_t)*CHAR_BIT), APInt(sizeof(intptr_t)*CHAR_BIT, Func));
 
-        IntToPtrInst* FuncAddrToPtrInst = new IntToPtrInst(fnptr, FunctionPtrTy);
-        IntToPtrInst* BPAddrToPtrInst = new IntToPtrInst(bpptr, VoidPointerTy);
+        IntToPtrInst* CallbackInst = new IntToPtrInst(CallbackVal, FunctionPtrTy);
+        IntToPtrInst* JPDInst      = new IntToPtrInst(JPDVal, VoidPointerTy);
+        IntToPtrInst* FuncInst     = new IntToPtrInst(FuncVal, VoidPointerTy);
 
         // Make the function call
         std::vector<Value*> ArrayRefVec;
-        ArrayRefVec.push_back(BPAddrToPtrInst);
-        CallInst* FuncCallInst = CallInst::Create(FuncAddrToPtrInst, ArrayRef<Value*>(ArrayRefVec));
-        F.getEntryBlock().getInstList().push_front(FuncCallInst);
-        F.getEntryBlock().getInstList().push_front(BPAddrToPtrInst);
-        F.getEntryBlock().getInstList().push_front(FuncAddrToPtrInst);
+        ArrayRefVec.push_back(JPDInst);
+        ArrayRefVec.push_back(FuncInst);
+        CallInst* FuncCall = CallInst::Create(CallbackInst, ArrayRef<Value*>(ArrayRefVec));
 
-        callBackInst.push_back(FuncCallInst);
-        callBackInst.push_back(BPAddrToPtrInst);
-        callBackInst.push_back(FuncAddrToPtrInst);
+        // Insert the instructions
+        F.getEntryBlock().getInstList().push_front(FuncCall);
+        F.getEntryBlock().getInstList().push_front(FuncInst);
+        F.getEntryBlock().getInstList().push_front(JPDInst);
+        F.getEntryBlock().getInstList().push_front(CallbackInst);
+
+        callBackInst.push_back(FuncCall);
+        callBackInst.push_back(FuncInst);
+        callBackInst.push_back(JPDInst);
+        callBackInst.push_back(CallbackInst);
 
         DEBUG( dbgs() << "[JITProfiling] Inserted Callback to " << F.getName() << "\n" );
-
       }
 
     private:
-      JITProfiling* JP;
+      JITProfilingData* JPD;
       SmallVector<Instruction*, 8>  callBackInst;
   };
 }
 
 char JITFunctionProfiling::ID = 0;
-static RegisterPass<JITFunctionProfiling> X("jitfprofiling", "Add/remove instruction for JIT online profiling of function", false, false);
+static RegisterPass<JITFunctionProfiling> X("jitfprofilingpass", "Add/remove instruction for JIT online profiling of function", false, false);
 
 namespace llvm {
-  FunctionPass *createJITFunctionProfilingPass(JITProfiling *jp) { return new JITFunctionProfiling(jp); }
+  FunctionPass *createJITFunctionProfilingPass(JITProfilingData* JPD) { return new JITFunctionProfiling(JPD); }
 }
