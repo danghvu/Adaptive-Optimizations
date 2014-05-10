c3f8ef960bd69ce59bb6d82dba324ad426279251
Alex Brooks <brooks8@illinois.edu>
2014-05-09 22:25:50 -0500

    Added script for running the llvm test-suite many times and combining results

---
+++ run.py
@@ -0,0 +1,179 @@
+#!/usr/bin/python
+
+import os
+import time
+import re
+from decimal import Decimal
+
+# data tuple:
+#  (name, passed?, native-time, jit-time)
+data = []
+
+# (t1, t2, avg_best)
+best = []
+# (t1, t2, avg_worst)
+worst = []
+# (t1. t2. avg_total)
+total = []
+
+def main():
+  t1_start = 4
+  t1_end   = 4
+  t2_start = 8
+  t2_end   = 8
+
+  remove_output()
+
+  for i in range(t1_start, t1_end+1):
+    for j in range(t2_start, t2_end+1):
+      print(str(i) + " - " + str(j))
+      del data[0:len(data)]
+      cmd = 'make -j 8 TEST=nightly EXTRA_LLIFLAGS="-enable-online-profile -t1 ' + str(i) + ' -t2 ' + str(j) + '" DISABLE_LLC=1 report.html'
+      print(cmd)
+      os.system(cmd)
+      t = int(round(time.time()))
+      parse_results(i, j, t)
+      output_data(i, j, t)
+      remove_output()
+
+  best.sort(key=lambda tup: tup[2])
+  worst.sort(key=lambda tup: -tup[2])
+
+  top = 5
+  if (len(best) < 5):
+    top = len(best)
+
+  print("*** Completed tests ***")
+  print(">>> Top configs [best avg] for (t1: " + str(t1_start) + ", t2: " + str(t2_start) + ") to (t1: " + str(t1_end) + ", t2: " + str(t2_end) + ")")
+  for i in range(0, top):
+    print("t1: " + str(best[i][0]) + " t2: " + str(best[i][1]) + "[" + str(best[i][2]) + "]")
+  print(">>> Top configs [worst avg] for (t1: " + str(t1_start) + ", t2: " + str(t2_start) + ") to (t1: " + str(t1_end) + ", t2: " + str(t2_end) + ")")
+  for i in range(0, top):
+    print("t1: " + str(worst[i][0]) + " t2: " + str(worst[i][1]) + "[" + str(worst[i][2]) + "]")
+  print(">>> Top configs [total avg] for (t1: " + str(t1_start) + ", t2: " + str(t2_start) + ") to (t1: " + str(t1_end) + ", t2: " + str(t2_end) + ")")
+  for i in range(0, top):
+    print("t1: " + str(total[i][0]) + " t2: " + str(total[i][1]) + "[" + str(total[i][2]) + "]")
+
+def parse_results(t1, t2, t):
+  f = open('report.nightly.raw.out', 'r')
+
+  while (get_next(f)):
+    continue
+
+  os.system("mv report.nightly.html results/report" + str(t1) + "_" + str(t2) + "_" + str(t) + ".html")
+  os.system("mv report.nightly.raw.out results/report" + str(t1) + "_" + str(t2) + "_" + str(t) + ".raw.out")
+  f.close()
+
+def remove_output():
+  os.system("find . -name *.out-jit | xargs -n 1 rm")
+
+def output_data(t1, t2, t):
+  failed = []
+  better = []
+  worse  = []
+
+  better_avg = 0.0
+  worse_avg  = 0.0
+  total_avg  = 0.0
+
+  for d in data:
+    if (not d[1]):
+      failed.append(d)
+    else:
+      if (d[2] > d[3]):
+        better.append(d)
+        better_avg += float(d[2] - d[3])
+      else:
+        worse.append(d)
+        worse_avg  += float(d[2] - d[3])
+      total_avg += float(d[2] - d[3])
+
+  better_avg = better_avg / len(better)
+  worse_avg  = worse_avg / len(worse)
+  total_avg  = total_avg / len(data)
+
+  worse.sort(key=lambda tup: tup[2] - tup[3])
+  better.sort(key=lambda tup: tup[3] - tup[2])
+
+  file_name = "results/data_" + str(t1) + "_" + str(t2) + "_" + str(t) + ".txt"
+  with open(file_name, "a") as myfile:
+    myfile.write("*** Failed tests ***\n")
+    for fail in failed:
+      myfile.write(fail[0] + "\n")
+
+    myfile.write("*** Better tests ***\n")
+    for b in better:
+      myfile.write(b[0] + " [" + str(b[2]) + ", " + str(b[3]) + "]\n")
+
+    myfile.write("*** Worse tests ***\n")
+    for w in worse:
+      myfile.write(w[0] + " [" + str(w[2]) + ", " + str(w[3]) + "]\n")
+
+    myfile.write("Better: " + str(better_avg) + "[" + str(len(better)) + "]\n")
+    myfile.write("Worse:  " + str(worse_avg) + "[" + str(len(worse)) + "]\n")
+    myfile.write("Total:  " + str(total_avg) + "[" + str(len(data)) + "]\n")
+
+    best.append((t1, t2, better_avg))
+    worst.append((t1, t2, worse_avg))
+    total.append((t1, t2, total_avg))
+
+#---------------------------------------------------------------
+#>>> ========= '/home/brooks8/LLVM/llvm-build/projects/test-suite/MultiSource/Applications/lua/lua' Program
+#---------------------------------------------------------------
+#
+#TEST-RESULT-nat-time: user       16.3785
+#
+#TEST-PASS: jit /home/brooks8/LLVM/llvm-build/projects/test-suite/MultiSource/Applications/lua/lua
+#TEST-RESULT-jit-time: user       26.6090
+#
+#TEST-RESULT-jit-comptime:   Total Execution Time: 0.2220 seconds (0.2219 wall clock)
+#
+#
+#TEST-PASS: compile /home/brooks8/LLVM/llvm-build/projects/test-suite/MultiSource/Applications/lua/lua
+#TEST-RESULT-compile:   Total Execution Time: 0.0410 seconds (0.0408 wall clock)
+#
+#TEST-RESULT-compile: 714216 Output/lua.llvm.bc
+#
+
+#---------------------------------------------------------------
+#>>> ========= '/home/brooks8/LLVM/llvm-build/projects/test-suite/MultiSource/Applications/minisat/minisat' Program
+#---------------------------------------------------------------
+#
+#TEST-RESULT-nat-time: user       4.7203
+#
+#TEST-FAIL: jit /home/brooks8/LLVM/llvm-build/projects/test-suite/MultiSource/Applications/minisat/minisat
+#
+#TEST-PASS: compile /home/brooks8/LLVM/llvm-build/projects/test-suite/MultiSource/Applications/minisat/minisat
+#TEST-RESULT-compile:   Total Execution Time: 0.0110 seconds (0.0116 wall clock)
+#
+#TEST-RESULT-compile: 82056 Output/minisat.llvm.bc
+#
+
+
+def get_next(f):
+  line = f.readline()
+  # if there is nothing on this line, then we are done reading the file
+  if (line == ''):
+    return False
+
+  match = re.search(r'\'([^&>]+)\'', f.readline())
+  name = match.group(1)
+  f.readline()
+  f.readline()
+  nat_time = Decimal(f.readline()[26:])
+  f.readline()
+  if (f.readline()[:9] == "TEST-PASS"):
+    jit_time = Decimal(f.readline()[26:])
+    dat = (name, True, nat_time, jit_time)
+    for _ in range(9):
+      f.readline()
+  else:
+    dat = (name, False, nat_time, -1)
+    for _ in range(6):
+      f.readline()
+
+  data.append(dat)
+  return True
+
+if __name__ == "__main__":
+  main()
